49223b5ae357f4b88e8bdc7403ff70bca1c490a4
Wwwzff <36884341+Wwwzff@users.noreply.github.com>
2018-11-24 22:32:19 -0500

    Update MiniProject3_mySQL.py

--- MiniProject3_mySQL.py
+++ MiniProject3_mySQL.py
@@ -111,10 +111,10 @@ def find_most_frequent_word(filename):
 
 path = "D:\\EC601\\"
 twitterID = 'linkinpark'
-consumer_key = "EaRJCL2n7WW7oGqZPGW8LjT2g"
-consumer_secret = "LpSj2RWRpW2MNTETotE6LIHizGyzdQNNS3vXOUEHDnwO0y07ws"
-access_token = "1038532509942857730-Sn24COVvc7Zt7rY6WM0GugU4JuUXr9"
-access_token_secret = "mpRtlWQtrw0YRdIkMUqHPoldxbDgpXSgxB4Vx6D3ybNOb"
+consumer_key = "***************"
+consumer_secret = "**************"
+access_token = "*************"
+access_token_secret = "**************"
 
 
 api = credentials(consumer_key,consumer_secret,access_token,access_token_secret)
